98d844a3618aa9a91fee93ebd119dc91050dbecb
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-20 00:29:15 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
+local bypass;
+    bypass = hookmetamethod(game, "__namecall", function(method, ...) 
+        if getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.Ban then
+            return
+        elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.AdminGUI then
+            return
+        elseif getnamecallmethod() == "FireServer" and method == game.ReplicatedStorage.WalkSpeedChanged then
+            return
+        end
+        return bypass(method, ...)
+    end)
+
+
 fireclickdetector(workspace.Lobby["Diamond"].ClickDetector)
 wait(0.5)
 game.ReplicatedStorage.Rockmode:FireServer()
